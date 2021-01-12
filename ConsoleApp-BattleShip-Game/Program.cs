81d724a59a4768e050e49da79ddd6b8a1093d90f
Elina_Sergeeva <elserg@ttu.ee>
2021-01-12 15:23:21 +0200

    Initial

---
+++ ConsoleApp-BattleShip-Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_BattleShip_Game
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
